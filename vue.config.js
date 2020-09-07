943ce8eb3519fdaa589a3261337689c79c076bdd
Xiawenqing <ar.xiawq@gmail.com>
2020-09-07 21:20:04 +0800

    v1.0.0

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+const path = require('path')
+
+module.exports = {
+  lintOnSave: false,
+  chainWebpack: (config) => {
+    config.resolve.alias
+      .set('@', path.join(__dirname, 'src'))
+      .set('@components', path.join(__dirname, 'src/components'))
+      .set('@static', path.join(__dirname, 'src/assets'))
+      .set('@views', path.join(__dirname, 'src/views'))
+  }
+}
